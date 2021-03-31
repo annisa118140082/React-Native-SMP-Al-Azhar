3115ed2c3a9967427ed256476c995b64cfec3fff
annisa118140082 <annisa.118140082@student.itera.ac.id>
2021-03-31 18:09:20 +0700

    halaman splashscreen

---
+++ src/SplashScreen/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {View, Image, StyleSheet} from 'react-native'
+import ImageSplashScreen from '../../assets/image/splashscreen.png'
+
+const SplashScreen = () => {
+    return (
+        <View style={styles.wrapper}>
+            <Image source={{uri: ImageSplashScreen}} style={styles.logo} />
+        </View>
+    );
+}
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+    wrapper: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white'
+    },
+    logo: {
+        width: 148,
+        height: 170
+    }
+})
