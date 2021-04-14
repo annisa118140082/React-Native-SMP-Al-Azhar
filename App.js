ef94e3ec3f306f2d6a3bd469590247eb8781d794
sofwan118140052 <sofwan.118140052@student.itera.ac.id>
2021-04-14 21:11:58 +0700

    update

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ export default function App() {
       {/* <Ekstrakulikuler /> */}
       {/* <TentangAplikasi /> */}
       {/* <Pengumuman /> */}
-      {/* <Prestasi /> */}
+      {/*. <Prestasi /> */}
     </View>
   );
 }
