3f1af0c80ffd6074f498f653be65b8d0976f5f3e
Sofwan86 <sofwan.hidayatnst@gmail.com>
2021-04-21 12:06:59 +0000

    update

--- App.js
+++ App.js
@@ -1,30 +1,32 @@
 import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 // import TentangAplikasi from './src/User/TentangAplikasi';
-import SplashScreen from './src/SplashScreen/index';
-import Home from './src/Home/index';
-// import ProfilSekolah from './src/User/ProfilSekolah/index';
+//import SplashScreen from './src/SplashScreen/index';
+import Home from './src/Home/';
+ import ProfilSekolah from './src/User/ProfilSekolah/index';
 // import InformasiPPDB from './src/User/InformasiPPDB/index';
-import Ekstrakulikuler from './src/User/Ekstrakulikuler/index'
+//import Ekstrakulikuler from './src/User/Ekstrakulikuler/index'
 // import Pengumuman from './src/User/Pengumuman/index'
-import TentangAplikasi from './src/User/TentangAplikasi'
-import Prestasi from './src/User/Prestasi';
+//import TentangAplikasi from './src/User/TentangAplikasi'
+//import Prestasi from './src/User/Prestasi';
+//import Admin from './src/Admin/AdminLogin';
 
 export default function App() {
-  const [splash, IsSplash] = useState(true)
-  const [home, IsHome] = useState(false)
-  useEffect(() => {
-    setTimeout(() => {
-      IsSplash(false)
-      IsHome(true)
-    }, 3000);
-  }, []);
+  // const [splash, IsSplash] = useState(true)
+  // const [home, IsHome] = useState(false)
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     IsSplash(false)
+  //     IsHome(true)
+  //   }, 3000);
+  // }, []);
 
   return (
     <View>
-      {splash && <SplashScreen />}
-      {home && <Home />}
-      {/* <ProfilSekolah /> */}
+      {/* {splash && <SplashScreen />}
+      {home && <Home />} */}
+    
+      { <ProfilSekolah /> }
       {/* <InformasiPPDB /> */}
       {/* <Ekstrakulikuler /> */}
       {/* <TentangAplikasi /> */}
